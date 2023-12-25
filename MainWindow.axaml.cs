ef369271c9484bf938b3462bcdf0aa585fa81d56
ggbb0711 <ggbotcute07@gmail.com>
2023-12-25 08:49:09 +0700

    Init commit

---
+++ MainWindow.axaml.cs
@@ -0,0 +1,13 @@
+using Avalonia.Controls;
+using System;
+
+namespace SnakeGame;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+        Console.WriteLine("Hello world");
+    }
+}
